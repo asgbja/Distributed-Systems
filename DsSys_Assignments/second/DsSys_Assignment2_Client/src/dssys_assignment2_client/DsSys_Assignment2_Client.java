11860f6f1e085d310fb96ec8049da9ad8775f216
dafarache <dafarache@debian.(none)>
2014-05-21 16:11:15 +0200

    some updates

--- DsSys_Assignments/second/DsSys_Assignment2_Client/src/dssys_assignment2_client/DsSys_Assignment2_Client.java
+++ DsSys_Assignments/second/DsSys_Assignment2_Client/src/dssys_assignment2_client/DsSys_Assignment2_Client.java
@@ -1,15 +1,43 @@
-
 package dssys_assignment2_client;
 
 import TXLFlightService.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 
 public class DsSys_Assignment2_Client {
-    
+
+    private static ClientGUI client;
+    private static JButton textButton;
+    private static String destination = "";
+
     public static void main(String[] args) {
-        TXLFlightPort port = new TXLFlight().getTXLFlightBinding();
+
         
-        int price = port.txlFlightcalc("MUC");
-        System.out.println(price);
+        initializeGUI();
+
     }
-    
+
+    private static void initializeGUI() {
+        client = new ClientGUI();
+        client.setVisible(true);
+
+        textButton = client.getButton();
+        textButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                destination = client.getDestinationField().getText();
+                doTheRest();
+            }
+        });
+
+    }
+
+    private static void doTheRest() {
+        System.out.println(destination);
+        TXLFlightPort port = new TXLFlight().getTXLFlightBinding();  
+        int price = port.txlFlightcalc(destination);
+        Result result = new Result(destination, price);
+        client.getTextField().setText(result.toString());
+    }
 }
